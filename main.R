573763b823d2203a0d9d3d21f27e7b79fcd0f2e1
liangchenn <r08323022@ntu.edu.tw>
2026-05-28 02:13:11 -0700

    feat: add entry point main script

---
+++ main.R
@@ -0,0 +1,30 @@
+#' README:
+#' -------
+#' - author: ...
+#' - date: 2026-05-27
+#' 
+#' Desc:
+#' -------
+#' This file is the main entry point of the project.
+#' Run this file and the codes will generate all results we include in our write-up.
+#' 
+#' Output
+#' --------
+#' -
+
+
+# Project Preparations ------------------------------------------------------------------------
+
+cat("Preparing project data: \n")
+source("scripts/01-load-raw-data.R")
+source("scripts/02-define-variables.R")
+source("scripts/03-construct-data.R")
+source("scripts/04-descriptive-stat.R")
+source("scripts/05-smd-love-plot.R")
+cat("finished \n\n")
+
+
+# Project Analysis ----------------------------------------------------------------------------
+
+cat("Preparing project analysis: \n\n")
+
